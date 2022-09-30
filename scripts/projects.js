7428e2a50384bb6b53eff692acb07ee52b1fe4a6
Romulo <romulosilva001@gmail.com>
2022-09-30 17:36:34 -0300

    Atualização seção projects - retirado botão projects all, deixando apenas front e back end

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,15 +1,16 @@
-const allBtn = document.querySelector('#all-btn')
+// const allBtn = document.querySelector('#all-btn')
 const frontBtn = document.querySelector('#front-btn');
 const backBtn = document.querySelector('#back-btn');
 const projectsFront = document.querySelector('#projects-front');
 const projectsBack = document.querySelector('#projects-back');
 
+
 frontBtn.addEventListener('click', () => {
-  projectsFront.classList.add('active');
+  projectsFront.style.display = 'flex';
   projectsBack.classList.remove('active');
 })
 
 backBtn.addEventListener('click', () => {
+  projectsFront.style.display = 'none';
   projectsBack.classList.add('active');
-  projectsFront.classList.remove('active');
 })
