ebde401840e1801ac1874021642f1537c18d291a
Romulo <romulosilva001@gmail.com>
2022-09-30 14:23:52 -0300

    Inclusão projetos back end na seção projetos e estilização da barra de rolagem

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,6 +1,15 @@
+const allBtn = document.querySelector('#all-btn')
 const frontBtn = document.querySelector('#front-btn');
+const backBtn = document.querySelector('#back-btn');
 const projectsFront = document.querySelector('#projects-front');
+const projectsBack = document.querySelector('#projects-back');
 
 frontBtn.addEventListener('click', () => {
-  projectsFront.classList.toggle('active');
+  projectsFront.classList.add('active');
+  projectsBack.classList.remove('active');
+})
+
+backBtn.addEventListener('click', () => {
+  projectsBack.classList.add('active');
+  projectsFront.classList.remove('active');
 })
